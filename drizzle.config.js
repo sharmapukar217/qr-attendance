71773d5f76308e6e85c211f10df13da49629d439
Pukar Sharma <sharmapukar217@gmail.com>
2024-05-02 17:16:48 +0545

    updated database to turso

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,10 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
   out: "./.drizzle",
-  driver: "libsql",
+  driver: "turso",
   schema: "./src/lib/database/schema.ts",
   dbCredentials: {
     url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_AUTH_TOKEN
   },
 };
